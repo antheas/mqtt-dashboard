5b4bf32313795fecab6ec349d64f55d6751425a8
antheas <antheas@users.noreply.github.com>
2020-07-26 20:12:59 +0000

    cleanup and make api generic

--- api/db.py
+++ api/db.py
@@ -6,16 +6,9 @@ from influxdb_client import InfluxDBClient
 
 
 class QueryResult:
-  def __init__(self, start, stop, topic, group, client, sensor, unit):
-    self.params = {
-        "start": start,
-        "stop": stop,
-        "topic": topic,
-        "group": group,
-        "client": client,
-        "sensor": sensor,
-        "unit": unit
-    }
+  def __init__(self, time, tags):
+    self.time = time
+    self.tags = tags
 
   def toDict(self):
     return self.__dict__
@@ -28,20 +21,24 @@ class Series(QueryResult):
   records = []
 
   def toDict(self):
-    return {"params": self.params, "records": self.records}
+    return self.__dict__
 
   def addRecord(self, time, val):
     self.records.append({"x": str(time), "y": val})
 
 
 class Discovery(QueryResult):
-  tags = []
+  values = []
+
+  def __init__(self, time, tags, search):
+    self.search = search
+    super().__init__(time, tags)
 
   def toDict(self):
-    return {"params": self.params, "tags": self.tags}
+    return self.__dict__
 
   def addValue(self, values):
-    self.tags.append(values)
+    self.values.append(values)
 
 
 class DatabaseManager:
@@ -50,21 +47,15 @@ class DatabaseManager:
     self.client = InfluxDBClient.from_env_properties()
     self.query_api = self.client.query_api()
 
-  def create_base_query(self, start="-15m", stop="now()", topic="", group="", client="", sensor="", unit=""):
-    return self.create_base_query_2(time={"start": start, "stop": stop}, tags={
-        "topic": topic,
-        "group": group,
-        "client": client,
-        "sensor": sensor,
-        "unit": unit
-    })
-
-  def create_base_query_2(self, time={"start": "-15m", "stop": "now()"}, tags={}):
+  def create_base_query(self, time={"start": "-15m", "stop": "now()"}, tags={}):
     query = 'from(bucket: "%s")' % (os.environ.get("INFLUXDB_BUCKET"))
 
     # Bound for time
-    start = time["start"]
-    stop = time["stop"]
+    if "start" in time:
+      start = time["start"]
+    if "stop" in time:
+      stop = time["stop"]
+
     if start and stop:
       query += '|> range(start: %s, stop: %s)' % (start, stop)
     elif start:
@@ -79,31 +70,35 @@ class DatabaseManager:
 
     return query
 
-  def query(self, start="-15m", stop="now()", topic="", group="", client="", sensor="", unit=""):
-    results = self.query_api.query(self.create_base_query(
-        start, stop, topic, group, client, sensor, unit))
-    series = Series(start, stop, topic, group, client, sensor, unit)
+  def query(self, time, tags):
+    results = self.query_api.query(self.create_base_query(time, tags))
+    series = Series(time, tags)
 
+    # Get results
     if not results:
       return series
     table = results[0]
+
+    # Append and return
     for record in table.records:
       series.addRecord(record.get_time(), record.get_value())
     return series
 
-  def tag_values(self, tag, start="-24h", stop="now()", topic="", group="", client="", sensor="", unit=""):
-    base = self.create_base_query(
-        start, stop, topic, group, client, sensor, unit)
+  def discovery(self, time, tags, search):
+    base = self.create_base_query(time, tags)
     query = base + \
         '|> group(columns:["%s"])|> distinct(column:"%s")|> group()' % (
-            tag, tag)
-    print(query)
-    results = self.query_api.query(query)
-    tags = Discovery(start, stop, topic, group, client, sensor, unit)
+            search, search)
 
+    results = self.query_api.query(query)
+    values = Discovery(time, tags, search)
+
+    # Get results
     if not results:
-      return tags.toJSON()
+      return values
     table = results[0]
+
+    # Append and return
     for record in table.records:
-      tags.addValue(record.get_value())
-    return tags.toJSON()
+      values.addValue(record.get_value())
+    return values
--- api/entry.py
+++ api/entry.py
@@ -11,12 +11,24 @@ def hello():
 
 @app.route("/query")
 def query():
-  return db.query("-1h", "now()", "", "temperature_sensor").toJSON()
+  return db.query(time={"start": "-1h", "stop": "now()"}, tags={
+      # "topic": = "",
+      # "group": = "",
+      # "client": = "",
+      # "sensor": = "",
+      # "unit": = ""
+  }).toJSON()
 
 
 @app.route("/discovery")
 def discovery():
-  return db.tag_values("group")
+  return db.discovery(time={"start": "-1h", "stop": "now()"}, tags={
+      # "topic": = "",
+      # "group": = "",
+      # "client": = "",
+      # "sensor": = "",
+      # "unit": = ""
+  }, search="sensor").toJSON()
 
 
 # print(db.query("-1h", "now()", "", "temperature_sensor").toJSON())
